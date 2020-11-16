423e5b5765e36c91e5767064c92c4f044ac3e176
RICARDO VACA <ricardo.vaca@pucese.edu.ec>
2020-11-16 09:55:52 -0500

    Agregar archivos de proyecto.

---
+++ FormularioApp/frmSueldo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormularioApp
+{
+    public partial class frmSueldo : Form
+    {
+        public frmSueldo()
+        {
+            InitializeComponent();
+        }
+    }
+}
